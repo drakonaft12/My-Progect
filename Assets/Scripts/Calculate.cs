afde8f28ea94a9eaedbb1133885d2f7ff014efac
drakonaft12 <61424327+drakonaft12@users.noreply.github.com>
2023-12-07 22:08:27 +0300

    New calcul

--- Assets/Scripts/Calculate.cs
+++ Assets/Scripts/Calculate.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Calculate : MonoBehaviour
 {
+    private enum Operations
+    {
+        Sum,
+        Sub,
+        Multi,
+        Div
+    }
+
     [SerializeField] private float _number1;
     [SerializeField] private float _number2;
-    [SerializeField] private bool _plus;
+    [SerializeField] private Operations _operations;
     [SerializeField] private bool _calculate = false;
     // Start is called before the first frame update
     void Start()
@@ -24,19 +32,36 @@ public class Calculate : MonoBehaviour
         var result = number1 - number2;
         return $"Result subtraction => {result}";
     }
+    private string MultiCalculate(float number1, float number2)
+    {
+        var result = number1 * number2;
+        return $"Result miltiplication => {result}";
+    }
+    private string DivCalculate(float number1, float number2)
+    {
+        var result = number1 / number2;
+        return $"Result division => {result}";
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (_calculate)
         {
-            if (_plus)
+            switch (_operations)
             {
-                Debug.Log(PlusCalculate(_number1, _number2));
-            }
-            else
-            {
-                Debug.Log(MinusCalculate(_number1, _number2));
+                case Operations.Multi:
+                    Debug.Log(MultiCalculate(_number1, _number2));
+                    break;
+                case Operations.Sum:
+                    Debug.Log(PlusCalculate(_number1, _number2));
+                    break;
+                case Operations.Sub:
+                    Debug.Log(MinusCalculate(_number1, _number2));
+                    break;
+                case Operations.Div:
+                    Debug.Log(DivCalculate(_number1, _number2));
+                    break;
             }
             _calculate = false;
         }
