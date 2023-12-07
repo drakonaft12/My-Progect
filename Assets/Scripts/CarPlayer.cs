485d5290853ee58ae802048cb35f28e772de8e46
drakonaft12 <61424327+drakonaft12@users.noreply.github.com>
2023-12-07 15:14:29 +0300

    Car rotate

--- Assets/Scripts/CarPlayer.cs
+++ Assets/Scripts/CarPlayer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class CarPlayer : MonoBehaviour
@@ -28,6 +29,11 @@ public class CarPlayer : MonoBehaviour
         {
             GetComponent<Rigidbody>().AddForce(transform.forward * 2000 * Input.GetAxis("Vertical"));
             GetComponent<Rigidbody>().AddForce(transform.up * 200 * Input.GetAxis("Vertical"));
+        }
+        if (Input.GetAxis("Horizontal") != 0f && Work)
+        {
+            Quaternion quaternion = Quaternion.Euler(new Vector3(0,1,0) * Input.GetAxis("Horizontal"));
+            GetComponent<Rigidbody>().MoveRotation(GetComponent<Rigidbody>().rotation * quaternion);
         }
     }
 }
